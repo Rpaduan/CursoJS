5c05ef24d831dd6ae627cbbc5b366cee451797d9
Rpaduan <renato_paduan@hotmail.com>
2026-01-24 17:54:34 -0300

    aula12

--- exercicios/aula12/ex012.js
+++ exercicios/aula12/ex012.js
@@ -1,6 +1,7 @@
 var agora = new Date()
 var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora} horas.`)
+var minuto = agora.getMinutes()
+console.log(`Agora são exatamente ${hora} horas e ${minuto} minutos.`)
 if (hora < 12) {
     console.log('Bom dia!')
 } else if (hora <= 18) {
