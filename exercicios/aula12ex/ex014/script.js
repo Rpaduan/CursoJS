66be2d77b7aacadc9f4e59c6b4abbce53e6a1430
Rpaduan <renato_paduan@hotmail.com>
2026-01-24 20:35:15 -0300

    ex hora do dia

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,1 +1,23 @@
-alert('Olá!')
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+var minuto = data.getMinutes()
+//var hora = 10
+msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
+if (hora >= 0 && hora < 12) {
+    //BOM DIA!
+    img.src = 'manha.png'
+    document.body.style.background = '#80C3F7'
+} else if (hora >= 12 && hora < 18) {
+    //BOA TARDE!
+    img.src = 'tarde.png'
+    document.body.style.background = '#DF823F'
+} else {
+    //BOA NOITE!
+    img.src = 'noite.png'
+    document.body.style.background = '#273F43'
+}
+}
+
